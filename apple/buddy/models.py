8df2a4928daa39dea6fbd303aa3bb801c70165df
an9wer <an9wer@gmail.com>
2017-09-20 23:51:47 +0800

    temp

--- apple/buddy/models.py
+++ apple/buddy/models.py
@@ -16,3 +16,15 @@ class age(models.Model):
     #name = models.EmailField(null=True, validators=[validate_email])
     #age = models.EmailField(validators=[validate_email])
 
+class Place(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=80)
+
+class Restaurant(models.Model):
+    place = models.OneToOneField(Place, on_delete=models.CASCADE)
+    serves_hot_dogs = models.BooleanField(default=False)
+    serves_pizza = models.BooleanField(default=False)
+
+class Waiter(models.Model):
+    restaurant = models.ForeignKey(Restaurant)
+    name = models.CharField(max_length=50)
