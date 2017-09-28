c9138064a7be72a12402987922b0b41350a99a7f
an9wer <an9wer@gmail.com>
2017-09-28 22:48:04 +0800

    temp

--- apple/apple/settings.py
+++ apple/apple/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'buddy',
-    'md'
+    'md',
 ]
 
 MIDDLEWARE = [
--- apple/md/models/fields/__init__.py
+++ apple/md/models/fields/__init__.py
@@ -1,3 +1,4 @@
-from .foreignkey import FK
+from .foreign_key import FK_One, FK_Many
+from .foreign_key_self import FK_Self
 from .many_to_many_field import M2M_Person, M2M_Group, M2M_Membership
 
