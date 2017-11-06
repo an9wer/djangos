51b20281abdf048d90f71cd132b940bd75b65b8e
an9wer <an9wer@gmail.com>
2017-11-06 21:04:38 +0800

    Add custom permission

--- apple/apple/settings.py
+++ apple/apple/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'forms_frame',
+    'authentication_tool',
 ]
 
 MIDDLEWARE = [
---
+++ apple/authentication_tool/models.py
@@ -0,0 +1,1 @@
+from .customization.models import CustomPermissionModel
